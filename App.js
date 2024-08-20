b26f4e590bd93bf71fc443931718bbd6a4aeedea
Kutik Gómez <144839359+Kutik-Gomez@users.noreply.github.com>
2024-08-20 11:09:09 -0500

    carpetas para desarrollo

--- App.js
+++ App.js
@@ -1,20 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomePagina from './components/HomePagina';
+import LoginPagina from './components/LoginPagina';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="LoginPagina">
+        <Stack.Screen name="LoginPagina" component={LoginPagina} />
+        <Stack.Screen name="HomePagina" component={HomePagina} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
