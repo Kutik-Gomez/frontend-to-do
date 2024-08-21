2e070325c12c3bc4066ce1de760ad28284a0cb16
Kutik Gómez <144839359+Kutik-Gomez@users.noreply.github.com>
2024-08-20 23:19:06 -0500

    ajuste de Proximos

--- screens/ProximosPagina.js
+++ screens/ProximosPagina.js
@@ -1,16 +1,24 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 import colors from "../styles/colors"; // Importa los colores globales
+import { FontAwesome } from '@expo/vector-icons';
+import ListaTareas from "../components/ListaTareas"; // Importa el componente ListaTareas
 
-export default function TareasPagina() {
+export default function TareasPagina({ navigation }) {
   return (
     <View style={styles.container}>
       <View style={styles.header}>
         <Text style={styles.title}>Próximos</Text>
       </View>
       <View style={styles.content}>
-        {/* Aquí puedes agregar el contenido de la página */}
+        <ListaTareas /> {/* Usa el componente ListaTareas */}
       </View>
+      <TouchableOpacity 
+        style={styles.fab} 
+        onPress={() => navigation.navigate("FormularioTarea")} 
+      >
+        <FontAwesome name="plus" size={24} color="white" />
+      </TouchableOpacity>
     </View>
   );
 }
@@ -18,23 +26,39 @@ export default function TareasPagina() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: colors.containerBackground, // Usa el color global para el fondo del contenedor
+    backgroundColor: colors.containerBackground,
   },
   header: {
-    backgroundColor: colors.headerBackground, // Usa el color global para el fondo del encabezado
+    backgroundColor: colors.headerBackground,
     paddingHorizontal: 16,
     paddingVertical: 20,
     borderBottomWidth: 1,
-    borderBottomColor: colors.headerBorderBottom, // Usa el color global para la línea inferior del encabezado
+    borderBottomColor: colors.headerBorderBottom,
   },
   title: {
     fontSize: 28,
     fontWeight: 'bold',
-    color: colors.titleColor, // Usa el color global para el texto del título
+    color: colors.titleColor,
   },
   content: {
     flex: 1,
-    backgroundColor: colors.contentBackground, // Usa el color global para el fondo del contenido
+    backgroundColor: colors.contentBackground,
     padding: 16,
+  },
+  fab: {
+    position: 'absolute',
+    width: 56,
+    height: 56,
+    alignItems: 'center',
+    justifyContent: 'center',
+    right: 16,
+    bottom: 16,
+    backgroundColor: '#007bff',
+    borderRadius: 28,
+    elevation: 5,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.8,
+    shadowRadius: 2,
   },
 });
